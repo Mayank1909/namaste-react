9673a8d9b3d757590d3f7318b3bc6904da7bd6d7
Mayank1909 <78377998+Mayank1909@users.noreply.github.com>
2023-10-02 18:25:32 +0530

    laying down the foundation

--- App.js
+++ App.js
@@ -1,22 +1,13 @@
-// suppose we have to creasgte nested divs like 
-{/* <div id="parent">
-    <div id="child">
-        <h1></h1>
-        <h2> </h2>
-    </div>
-</div> */}
-// if we are asked to create a siblings then we wi;; use [] to make it and pass all the createelemtn in it . 
-
-const parent = React.createElement("div", {
-    id: "parent"
-}, React.createElement("div", { id: "child" }, [React.createElement("h1", {}, "hello i am h1 in  nested div "), React.createElement("h2", {}, "hello i am h2 in nested div ")]))
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 
-const heading = React.createElement("h1",
-    { id: "heading", xyz: "abc" },  // whatever is passed its and attributes
-    "Hello world from react");
-// console.log(heading);
-console.log(parent);
+// React element 
+// const heading = React.createElement("h1", { id: "heading" }, "Namaste React 🚀"); // this an obejct
+
+//JSX thsi jsx code is transpiled before going to js by parcel bable
+const jsxheading = <h1 className="heading">Namaste React 🚀</h1>
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
-root.render(parent);
+
+root.render(jsxheading);
---
+++ README.md
@@ -0,0 +1,17 @@
+# namsate react
+
+
+# parcel
+- Dev build
+- local server
+- HMR Hot module Replacement
+- file watching algorithm which is written in c++
+- Caching - fadter build
+- minification
+- bundeling 
+- compressing 
+- diagnosting
+- error haneling 
+- https
+- tree shaiking - parcel will remove unused code for you
+- different buid for dev and production bundle
